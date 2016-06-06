2b3ae4e4e2e7206ce026f725b214172ef64359a5
Matt Ransford <matt.ransford@gmail.com>
2016-06-05 20:35:13 -0700

    the roughest of outlines

---
+++ stj.py
@@ -0,0 +1,13 @@
+# auth with jira
+# auth with google sheets
+
+# in sheets
+# read row one
+# copy string in each cell
+# read last row
+# copy string in each cell
+# match the corresponding columns
+# pair them up
+
+# format for jira
+# export to jira

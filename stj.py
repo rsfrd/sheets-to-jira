2b97d8cb0dbfd3d896ba8d17ca1d7f7170b7518f
rsfrd <matt.ransford@gmail.com>
2016-06-19 16:09:55 -0700

    detect most recent row

--- stj.py
+++ stj.py
@@ -4,7 +4,7 @@
 # in sheets
 # read row one - done!
 # copy string in each cell - done!
-# read last row - manual
+# read last row - done! 
 # copy string in each cell - done!
 # match the corresponding columns - done!
 # pair them up - done!
@@ -20,7 +20,7 @@ scope = ['https://spreadsheets.google.com/feeds']
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name('gspread-credentials-mr.json', scope)
 
-closed_row = raw_input("Most recent closing row number > ")
+# closed_row = raw_input("Most recent closing row number > ")
 
 gc = gspread.authorize(credentials)
 
@@ -28,8 +28,16 @@ sh = gc.open('sheets to jira test document')
 
 worksheet = sh.sheet1
 
+def row_start(vrow):
+    while worksheet.cell(vrow, 1).value != "":
+        vrow += 1            
+    else:
+        return vrow - 1 
+
+new_closing = row_start(1)
+
 survey_questions = filter(None, worksheet.row_values(1))
-survey_answers = filter(None, worksheet.row_values(closed_row))
+survey_answers = filter(None, worksheet.row_values(new_closing))
 
 for i in izip(survey_questions, survey_answers):
     print i[0]

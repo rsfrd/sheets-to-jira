b3ad5c6a984b8a9a78aa9c398ac278fceaeaba10
rsfrd <matt.ransford@gmail.com>
2016-06-19 16:25:58 -0700

    write to file

--- stj.py
+++ stj.py
@@ -20,8 +20,6 @@ scope = ['https://spreadsheets.google.com/feeds']
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name('gspread-credentials-mr.json', scope)
 
-# closed_row = raw_input("Most recent closing row number > ")
-
 gc = gspread.authorize(credentials)
 sh = gc.open('sheets to jira test document')
 worksheet = sh.sheet1
@@ -37,22 +35,9 @@ new_closing = row_start(1)
 survey_questions = filter(None, worksheet.row_values(1))
 survey_answers = filter(None, worksheet.row_values(new_closing))
 
+jira = open('closing-jira.txt', 'w+')
+
 for i in izip(survey_questions, survey_answers):
-    print i[0]
-    print '- ' + i[1] + '\n'
+    jira.write(i[0] + '\n')
+    jira.write('- ' + i[1] + '\n\n')
 
-
-# q_and_a = zip(survey_questions, survey_answers)
-# 
-# print q_and_a
-# 
-# q_and_a_len = len(q_and_a)
-# for i in range (0, q_and_a_len):
-#     print q_and_a[i]
- 
-
-# print "%s" % q_and_a[0][0]
-# print "- %s" % q_and_a[0][1]
-# print ""
-# print "%s" % q_and_a[1][0]
-# print "- %s" % q_and_a[1][1]

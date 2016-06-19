f31f8b124609e869fbfbad05a589303250ef6cb2
rsfrd <matt.ransford@gmail.com>
2016-06-19 00:01:58 -0700

    progress\!

--- stj.py
+++ stj.py
@@ -21,5 +21,22 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('gspread-credenti
 
 gc = gspread.authorize(credentials)
 
-wks = gc.open('sheets to jira test document').sheet1
+sh = gc.open('sheets to jira test document')
 
+worksheet = sh.sheet1
+
+survey_questions = worksheet.row_values(1)
+survey_answers = worksheet.row_values(4)
+
+print survey_questions
+print survey_answers
+
+q_and_a = zip(survey_questions, survey_answers)
+
+print q_and_a
+
+print "%s" % q_and_a[0][0]
+print "- %s" % q_and_a[0][1]
+print ""
+print "%s" % q_and_a[1][0]
+print "- %s" % q_and_a[1][1]

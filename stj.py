e3ba8effd48b29dacc8cacab09df781f4f235e87
rsfrd <matt.ransford@gmail.com>
2016-06-19 10:27:44 -0700

    copuling and formatting

--- stj.py
+++ stj.py
@@ -1,17 +1,18 @@
 # auth with jira
-# auth with google sheets
+# auth with google sheets - done!
 #
 # in sheets
-# read row one
-# copy string in each cell
-# read last row
-# copy string in each cell
-# match the corresponding columns
-# pair them up
+# read row one - done!
+# copy string in each cell - done!
+# read last row - manual
+# copy string in each cell - done!
+# match the corresponding columns - done!
+# pair them up - done!
 #
-# format for jira
+# format for jira - done!
 # export to jira
 
+from itertools import *
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -25,18 +26,25 @@ sh = gc.open('sheets to jira test document')
 
 worksheet = sh.sheet1
 
-survey_questions = worksheet.row_values(1)
-survey_answers = worksheet.row_values(4)
+survey_questions = filter(None, worksheet.row_values(1))
+survey_answers = filter(None, worksheet.row_values(4))
 
-print survey_questions
-print survey_answers
+for i in izip(survey_questions, survey_answers):
+    print i[0]
+    print '- ' + i[1] + '\n'
 
-q_and_a = zip(survey_questions, survey_answers)
 
-print q_and_a
+# q_and_a = zip(survey_questions, survey_answers)
+# 
+# print q_and_a
+# 
+# q_and_a_len = len(q_and_a)
+# for i in range (0, q_and_a_len):
+#     print q_and_a[i]
+ 
 
-print "%s" % q_and_a[0][0]
-print "- %s" % q_and_a[0][1]
-print ""
-print "%s" % q_and_a[1][0]
-print "- %s" % q_and_a[1][1]
+# print "%s" % q_and_a[0][0]
+# print "- %s" % q_and_a[0][1]
+# print ""
+# print "%s" % q_and_a[1][0]
+# print "- %s" % q_and_a[1][1]

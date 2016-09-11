a3439df4ec1a93c07eef3d318e0a58e428fb1694
Matt Ransford <matt.ransford@gmail.com>
2016-09-11 08:50:02 -0700

    added pem to read key file

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ copy requirements.txt /requirements.txt
 # RUN apt-get python-slugify
 # RUN apk add --update gcc musl-dev libffi-dev libedit-dev bash openssl-dev readline
 RUN pip install -r /requirements.txt
-RUN pip install python-slugify
+RUN pip install python-slugify pem
--- survey-mt-reorder.py
+++ survey-mt-reorder.py
@@ -7,14 +7,18 @@ from jira import JIRA
 from itertools import izip
 from oauth2client.service_account import ServiceAccountCredentials
 from slugify import slugify
-
+import pem
 
 def get_jira_client():
     """ Return a connected JIRA API client, configured by environment """
     options = {
         'server': os.environ.get('SERVER')
     }
-    key_cert_data = os.environ.get('JIRA_KEY')
+
+    key_cert_data = pem.parse_file("mt-jira.pem")
+    key_cert_data = str(key_cert_data[0])
+    
+    # key_cert_data = os.environ.get('JIRA_KEY')
     # key_cert_data = open('mt-jira.pem', 'r')
 
     oauth_dict = {
